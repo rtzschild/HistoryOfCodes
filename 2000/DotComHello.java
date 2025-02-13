c4dddcaeecaeef89fb8cf73c4c8befb60eb2a416
Susan :) <69177346+rtzschild@users.noreply.github.com>
2025-02-13 15:50:31 -0500

    Create DotComHello.java

---
+++ 2000/DotComHello.java
@@ -0,0 +1,14 @@
+/*
+ * DotComHello.java
+ *
+ * This Java program exemplifies the widespread use of Java during the dot-com boom.
+ * In the early 2000s, Java was widely adopted for building web applications and enterprise software.
+ * At this time, web technologies like HTML, CSS, and JavaScript were coming into their own,
+ * and Java provided a robust backend solution.
+ */
+
+public class DotComHello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dot-Com Era! Welcome to the early web revolution.");
+    }
+}
