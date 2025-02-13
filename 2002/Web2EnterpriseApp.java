79eaeeabbef4ecb82e61f26141a4b92449c590df
Susan :) <69177346+rtzschild@users.noreply.github.com>
2025-02-13 15:52:20 -0500

    Create Web2EnterpriseApp.java

---
+++ 2002/Web2EnterpriseApp.java
@@ -0,0 +1,14 @@
+/*
+ * Web2EnterpriseApp.java
+ *
+ * This simple Java program illustrates the continued dominance of Java in enterprise applications
+ * during the Web 2.0 era. As interactive, user-generated web content emerged and broadband internet
+ * provided faster data transfer, Java powered robust backend systems that supported dynamic websites.
+ */
+
+public class Web2EnterpriseApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Web 2.0 Era!");
+        System.out.println("Java continues to power enterprise solutions with rich interactive experiences.");
+    }
+}
